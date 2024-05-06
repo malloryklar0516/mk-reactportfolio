4c28f97d68a306114b1fb6904acf78f07a62ac83
malloryklar0516 <malloryklar@Mallorys-MacBook-Air.local>
2024-05-06 15:53:16 -0400

    project updates

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,30 +8,31 @@ function Portfolio(){
     const projects = [ 
         {
             projectName: "New Movie Dashboard",
-            description: "Front-end application that uses third party APIs to display information and trailers for new releases. Application functionality also includes ability users to add movies to their 'must see list' using local storage. ",
-            projectLink:  "https://vishalmanglani7.github.io/project-1-front-end/",
-            repoLink: "https://vishalmanglani7.github.io/project-1-front-end/",
+            description: "Front-end application that uses third party APIs to display information and trailers for a curated set of new releases. Application functionality also includes ability users to add movies to their 'must-see list' using local storage. ",
+            projectLink:  "https://malloryklar0516.github.io/moviedash/",
+            repoLink: "https://github.com/malloryklar0516/moviedash/",
             projectImg: movie
         },
         {
-            projectName: "City Weather Dashboard",
-            description: "Interactive front-end application that allows a user to search by city, and displays current weather and 5-day forecasts",
-            projectLink: "https://heroku.com/",
+            projectName: "Weather Dashboard",
+            description: "An application that displays current weather and the five-day weather forecast for a city based on the user’s search query. The application updates dynamically in HTML and CSS by search.",
+            projectLink: "https://malloryklar0516.github.io/weather-dashboard/",
             repoLink: "https://github.com/malloryklar0516/weather-dashboard",
             projectImg: weather
 
         },
         {projectName: "Fitness Tracker (HTML/CSS/JS)",
-            description: "Interactive full-stack application that allows users to create an account that they can use to store and track their fitness progress.",
-            projectLink: "https://heroku.com/",
-            repoLink: "https://github.com/VishalManglani7/FitnessApp",
+            description: "An interactive, full-stack application that allows users to create an account, sign in/log out, and track their fitness progress.",
+            projectLink: "https://track-my-fitness-46708e767d62.herokuapp.com",
+            repoLink: "https://github.com/malloryklar0516/fitnesstracker",
             projectImg: fitness
-    }, 
-    {projectName: "Book App",
-    description: "Full-stack application built using MERN stack, that allows users to sign up/log in and browse through a collection of books with user reactions, and can add their own books with their reaction to the interactive library.",
-    projectLink: "None",
-    repoLink: "https://github.com/malloryklar0516/SVG-logo",
-    projectImg: book}
+    }
+    // , 
+    // {projectName: "Book App",
+    // description: "Full-stack application built using MERN stack, that allows users to sign up/log in and browse through a collection of books with user reactions, and can add their own books with their reaction to the interactive library.",
+    // projectLink: "None",
+    // repoLink: "https://github.com/malloryklar0516/SVG-logo",
+    // projectImg: book}
     ];
     return (
         <div className="d-flex">
